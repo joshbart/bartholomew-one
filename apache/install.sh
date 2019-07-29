516fcdebca8ebf035d8ecf41920ec3d9658bcca3
Joshua Bartholomew <joshua.bartholomew@realtruck.com>
2019-07-29 07:34:34 -0500

    Added initial development stuffs.

--- apache/install.sh
+++ apache/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+### Install Apache ###
 apt-get update
 apt-get -y install apache2
+
+### Link development folder with site folder ###
+ln -s /vagrant/bartholomew.one /var/www/
+
+### Activate updated Apache config ###
 cp -f /vagrant/apache/000-default.conf /etc/apache2/sites-available/
+service apache2 restart
